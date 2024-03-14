c79a205960e74942b99d2ff36c825e6394188a94
Bela Jash <abelxotessema@gmail.com>
2024-03-14 17:59:59 +0300

    Add a NetworkImage to the body of the Scaffold.

--- lib/main.dart
+++ lib/main.dart
@@ -8,9 +8,16 @@ void main() {
         backgroundColor: Colors.blueGrey,
         // A component widget
         appBar: AppBar(
+          backgroundColor: Colors.blueGrey[900],
           title: Text(
             'I Am Rich',
             style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://img.freepik.com/free-photo/natures-beauty-captured-colorful-flower-close-up-generative-ai_188544-8593.jpg'),
           ),
         ),
       ),
